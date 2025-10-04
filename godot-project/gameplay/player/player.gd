fe715bd0992b54f18582a7ff1e0b98bcd7b03841
moisesjbc <moisesjbc@gmail.com>
2025-10-04 12:14:21 +0100

    Add player movement

---
+++ godot-project/gameplay/player/player.gd
@@ -0,0 +1,13 @@
+extends CharacterBody2D
+
+var SPEED: int = 500
+
+func _process(delta: float) -> void:
+	var direction: Vector2 = Vector2.ZERO
+
+	if Input.is_action_pressed("ui_left"):
+		direction.x = -1
+	elif Input.is_action_pressed("ui_right"):
+		direction.x = 1
+
+	move_and_collide(direction * SPEED * delta)
