7cad1f9aaacdb9e9b1b2b94ec0b4fbd083b9b74a
moisesjbc <moisesjbc@gmail.com>
2025-10-04 14:21:38 +0100

    Add game countdown

--- godot-project/gameplay/gui/gui.gd
+++ godot-project/gameplay/gui/gui.gd
@@ -1,14 +1,27 @@
 extends Node
 
+func _ready():
+	update_sell_label(0, "???", 0)
+	update_score_label(0)
+	update_time_label(get_parent().seconds_left)
 
 func update_sell_label(quantity: int, type: String, money: int) -> void:
 	$control/margin_container/bottom_container/sell_label.text = "Sell " + str(quantity) + " " + type + " for " + str(money) + " €"
 
 func update_score_label(score: int) -> void:
 	$control/margin_container/top_container/score_label.text = "Money: " + str(score)
+	
+func update_time_label(seconds_left: int) -> void:
+	$control/margin_container/top_container/time_label.text = "Time: " + str(seconds_left)
 
 func _on_player_collectibles_updated(quantity: int, type: String, money: int) -> void:
 	update_sell_label(quantity, type, money)
 
 func _on_player_money_updated(money: int) -> void:
 	update_score_label(money)
+
+func _on_main_time_elapsed(seconds_left: int) -> void:
+	update_time_label(seconds_left)
+
+func _on_main_game_over() -> void:
+	update_time_label(0)
---
+++ godot-project/gameplay/main/main.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+signal time_elapsed(seconds_left: int)
+signal game_over
+
+var seconds_left: int = 10
+
+
+func _on_countdown_timer_timeout() -> void:
+	seconds_left -= 1
+	if seconds_left > 0:
+		emit_signal("time_elapsed", seconds_left)
+	else:
+		emit_signal("game_over")
