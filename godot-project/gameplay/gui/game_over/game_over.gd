dec4118347c3e25fd836d30eef41b2758b1dc2ff
moisesjbc <moisesjbc@gmail.com>
2025-10-05 09:55:58 +0100

    Add main menu

--- godot-project/gameplay/gui/game_over/game_over.gd
+++ godot-project/gameplay/gui/game_over/game_over.gd
@@ -6,9 +6,16 @@ func start() -> void:
 	visible = true
 
 func restart_game() -> void:
+	unpause()
+	get_tree().reload_current_scene()
+
+func main_menu() -> void:
+	unpause()
+	get_tree().change_scene_to_file("res://menus/main_menu/main_menu.tscn")
+
+func unpause() -> void:
 	visible = false
 	get_tree().paused = false
-	get_tree().reload_current_scene()
 
 func _on_restart_game_button_pressed() -> void:
 	restart_game()
@@ -16,3 +23,6 @@ func _on_restart_game_button_pressed() -> void:
 func _input(event: InputEvent) -> void:
 	if event is InputEventKey and event.keycode == KEY_R:
 		restart_game()
+
+func _on_main_menu_button_pressed() -> void:
+	main_menu()
