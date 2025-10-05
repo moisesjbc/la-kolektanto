b399130efeaa49a2900ba4dfc325c1f06eb3c48d
moisesjbc <moisesjbc@gmail.com>
2025-10-05 16:14:46 +0100

    Add exit button to main menu

--- src/menus/main_menu/main_menu.gd
+++ src/menus/main_menu/main_menu.gd
@@ -1,6 +1,9 @@
 extends Control
 
 
+func _ready() -> void:
+	$margin_container/vbox_container/exit_button.visible = (OS.get_name() != "Web")
+
 func _on_play_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://gameplay/main/main.tscn")
 
@@ -9,3 +12,6 @@ func _on_credits_button_pressed() -> void:
 
 func _on_tutorial_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://gameplay/tutorial/tutorial.tscn")
+
+func _on_exit_button_pressed() -> void:
+	get_tree().quit()
